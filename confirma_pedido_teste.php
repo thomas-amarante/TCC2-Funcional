92ac0801b78fc7723de7dc85c64897287150741b
thomasamarnate <thomasferreiraa@gmail.com>
2018-05-25 19:05:20 -0300

    Ajuste de pagina de confimação de pagamento

--- confirma_pedido_teste.php
+++ confirma_pedido_teste.php
@@ -24,163 +24,87 @@
 	}
 	  
 ?>
-
 <!DOCTYPE html>
-<html>
+<html xmlns="http://www.w3.org/1999/xhtml">
 
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" type="text/css">
+ <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+ <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" type="text/css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.7.1/css/bootstrap-datepicker.standalone.min.css">
   <link rel="stylesheet" href="now-ui-kit.css" type="text/css">
   <link rel="stylesheet" href="assets/css/nucleo-icons.css" type="text/css">
   <script src="assets/js/navbar-ontop.js"></script>
-  
-  <link rel="icon" href=""> <!-- adicionar ícone aqui -->
-  
-  <title>Marquejá</title>
-  
-  
-  
-  <script>
-	// funcão para automatizar e atribuir um valor a variável 'code', pegando esse valor via post do arquivo pagseguro.php
-	 function enviaPagseguro(codigo){
+ <title>Pedido de teste</title>
  
-		$.post('salvarPedido.php','',function(idPedido){
+<script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>﻿
+</head>
+
+<body>
  
-		$('#loading').css("visibility","visible");
+    
+ <div class="container py-5 ">
+	
+ </div>
+ <div class="container">
+	 <div class="row">
+		 <div class="col">
+			<div class="card">
+				<div class="card-header">
+					Confirma os dados da sua reserva:
+				</div>
+				<div class="card-body">
+							<p class="text-center">Data selecionada: <?php echo $data ?></p>
+					
+							<p class="text-center">Quadra selecionada: <?php echo $quadra ?></p>
+					
+							<p class="text-center">Horário do jogo: <?php echo $hora ?></p>
+					
+							<p class="text-center">Usuário: <?php echo $_SESSION['NOME'] ?></p>
+					
+							<p class="text-center">Valor: R$<?php echo $valor ?></p>	
+				</div>
+				<div class="card-footer"> Efetuar pagamento:
+				<button class="btn btn-success"  onclick="enviaPagseguro();">Comprar</button>  <!-- botão com função ao clicar: executar a função enviaPagseguro -->
+				</div>
+				<div class="card-footer"> Pagar no ato:
+				<button class="btn btn-info" >Pagar no ato</button>  <!-- botão com função ao clicar: executar a função enviaPagseguro -->
+				</div>
+			<div>
+		 </div>
+	 </div>
+ </div>
  
-		$.post('pagseguro.php',{idPedido: idPedido},function(data){
 
-		$('#code').val(data);
-		$('#comprar').submit();
+ 
+ <img src="loading.gif" id="loading" style="visibility: hidden">
+ 
+ <form id="comprar" action="https://sandbox.pagseguro.uol.com.br/v2/checkout/payment.html" method="post" onsubmit="PagSeguroLightbox(this); return false;">
+ <input type="hidden" name="code" id="code" value="" /> 
+ </form>
 
-		$('#loading').css("visibility","hidden");
-			})
-		})
-	}
-	var date = new Date();
-	date.setDate(date.getDate());
-	
-    $(document).ready(function(){
-            $('[data-toggle="popover"]').popover();   
-            $('[data-toggle="tooltip"]').tooltip();
-            $('#datepicker-example').datepicker({
-                calendarWeeks: true,
-                autoclose: true,
-                todayHighlight: true,
-				startDate: date
-				
-            });
-          });
-		  
-		
-  </script>
-  
+ <script type="text/javascript" src="https://stc.sandbox.pagseguro.uol.com.br/pagseguro/api/v2/checkout/pagseguro.lightbox.js"></script> <!-- js necessário para execução da lightbox -->
+ <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+ 
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
 
-<body class="">
-  <nav class="navbar navbar-expand-lg bg-primary navbar-dark fixed-top">
-    <div class="container">
-      <a class="navbar-brand" title="#marquejá" data-placement="bottom" data-toggle="tooltip" href="#firststep">#MarqueJá</a>
-      <button class="navbar-toggler navbar-toggler-left" type="button" data-toggle="collapse" data-target="#navbarNowUIKitFree">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse text-center justify-content-end" id="navbarNowUIKitFree">
-        <ul class="navbar-nav">
-          <li class="nav-item mx-1 align-items-center d-flex justify-content-center">
-            <a class="nav-link" href="#download">
-              <i class="now-ui-icons arrows-1_cloud-upload-94 x2 mr-2"></i>&nbsp;DOWNLOAD</a>
-          </li>
-          <li class="nav-item mx-1 align-items-center d-flex justify-content-center">
-            <a class="nav-link" href="#">
-              <i class="now-ui-icons files_paper x2 mr-2"></i>COMPONENTS</a>
-          </li>
-        </ul>
-        <a class="btn btn-light text-primary" href="#pro">
-          <i class="now-ui-icons arrows-1_share-66 mr-1"></i>Upgrade to PRO</a>
-        <ul class="navbar-nav flex-row justify-content-center mt-2 mt-md-0">
-          <li class="nav-item mx-1">
-            <a class="nav-link" href="#" data-placement="bottom" data-toggle="tooltip" title="Follow us on Twitter">
-              <i class="fa fa-fw fa-twitter fa-2x"></i>
-            </a>
-          </li>
-          <li class="nav-item mx-3 mx-md-1">
-            <a class="nav-link" href="#" data-placement="bottom" data-toggle="tooltip" title="Like us on Facebook">
-              <i class="fa fa-fw fa-facebook-official fa-2x"></i>
-            </a>
-          </li>
-          <li class="nav-item ml-1">
-            <a class="nav-link" href="#" data-placement="bottom" data-toggle="tooltip" title="Follow us on Instagram">
-              <i class="fa fa-fw fa-instagram fa-2x"></i>
-            </a>
-          </li>
-		  <li class="nav-item mx-1 align-items-center d-flex justify-content-center">
-            <a class="nav-link" href="logoff.php">
-              <i class="now-ui-icons files_paper x2 mr-2"></i>Sair</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
-  <div>
-  <div class="container py-5 bg-success">
-	
-  </div>
-  <div class="py-5 bg-secondary">
-	<div class="container">
-      <div class="row">
-        <div class="col-md-12">
-			  <p class="text-center">Data selecionada: <?php echo $data ?></p>
-		</div>
-	  </div>
-	  <div class="row">
-        <div class="col-md-12">
-		<p class="text-center">Quadra selecionada: <?php echo $quadra ?></p>
-		</div>
-	  </div>
-	  <div class="row">
-        <div class="col-md-12">
-		 <p class="text-center">Horário do jogo: <?php echo $hora ?></p>
-		</div>
-	  </div>
-	  <div class="row">
-        <div class="col-md-12">
-		 <p class="text-center">Usuário: <?php echo $_SESSION['NOME'] ?></p>
-		</div>
-	  </div>
-	  <div class="row">
-        <div class="col-md-12">
-		 <p class="text-center">Valor: R$<?php echo $valor ?></p>
-		</div>
-	  </div>
-	</div>
-  </div>
-    <div class="container py-5 bg-primary">
-      <div class="row">
-        <div class="col-md-6">
-          <h4 class="mb-3">Confirmar pedido?</h4>
-        </div>
-	
-		<button class="btn btn-success" onclick="enviaPagseguro()">Comprar</button>
-	
-	
-		<form id="comprar" action="https://sandbox.pagseguro.uol.com.br/v2/checkout/payment.html" method= "post" onsubmit="PagSeguroLightbox(this); return false;">
-			<input type="hidden" name="code" id="code" value="" />
-		</form>
-		
-      </div>
-    </div>
-  </div>
-     <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>﻿
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-  <script src="assets/js/parallax.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.7.1/js/bootstrap-datepicker.js"></script>
-  <script type="text/javascript"src="https://stc.sandbox.pagseguro.uol.com.br/pagseguro/api/v2/checkout/pagseguro.lightbox.js"></script>
-  
-  
-  
-  </body>
+ <script>
+ function enviaPagseguro(codigo){ <!-- função que salva o pedido no banco, recupera a coluna id e passa via post para a pagina pagseguro.php, além de passar dinamicamente a variável code junto com a url do form acima -->
+ 
+   $.post('salvarPedido.php','',function(idPedido){
+ 
+     $('#loading').css("visibility","visible");
+ 
+     $.post('pagseguro.php',{idPedido:idPedido},function(data){
 
-</html>
+       $('#code').val(data);
+       $('#comprar').submit();
+
+       $('#loading').css("visibility","hidden");
+     })
+   })
+ }
+ </script>
+ 
+</body>
+</html>	
